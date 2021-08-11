68889e0b2edc158d486beb879be9a5b14679a72d
adrianlukito <lukito.adrian1@gmail.com>
2021-08-11 15:09:51 +0700

    Account ViewModel, State, Repository, ApiService

---
+++ app/src/main/java/com/codingwithmitch/openapi/repository/main/AccountRepository.kt
@@ -0,0 +1,22 @@
+package com.codingwithmitch.openapi.repository.main
+
+import android.util.Log
+import com.codingwithmitch.openapi.api.main.OpenApiMainService
+import com.codingwithmitch.openapi.persistence.AccountPropertiesDao
+import com.codingwithmitch.openapi.session.SessionManager
+import kotlinx.coroutines.Job
+import javax.inject.Inject
+
+class AccountRepository @Inject constructor(
+    val openApiMainService: OpenApiMainService,
+    val accountPropertiesDao: AccountPropertiesDao,
+    val sessionManager: SessionManager
+) {
+    private val TAG = "AppDebug"
+
+    private var repositoryJob: Job? = null
+
+    fun cancelActiveJobs() {
+        Log.d(TAG, "AccountRepository: cancelling on-going jobs")
+    }
+}
