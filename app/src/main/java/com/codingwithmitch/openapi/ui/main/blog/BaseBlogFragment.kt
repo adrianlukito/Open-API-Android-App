9d26422163202ef6ede0e49d0e47fbaaf65af1bc
adrianlukito <lukito.adrian1@gmail.com>
2021-08-17 17:55:02 +0700

    AreYouSure Dialog

--- app/src/main/java/com/codingwithmitch/openapi/ui/main/blog/BaseBlogFragment.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/main/blog/BaseBlogFragment.kt
@@ -12,6 +12,7 @@ import androidx.navigation.ui.NavigationUI
 import com.bumptech.glide.RequestManager
 import com.codingwithmitch.openapi.R
 import com.codingwithmitch.openapi.ui.DataStateChangeListener
+import com.codingwithmitch.openapi.ui.UICommunicationListener
 import com.codingwithmitch.openapi.ui.main.blog.viewmodel.BlogViewModel
 import com.codingwithmitch.openapi.viewmodels.ViewModelProviderFactory
 import dagger.android.support.DaggerFragment
@@ -30,6 +31,8 @@ abstract class BaseBlogFragment : DaggerFragment(){
     lateinit var viewModel: BlogViewModel
 
     lateinit var stateChangeListener: DataStateChangeListener
+
+    lateinit var uiCommunicationListener: UICommunicationListener
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -57,6 +60,12 @@ abstract class BaseBlogFragment : DaggerFragment(){
             stateChangeListener = context as DataStateChangeListener
         }catch(e: ClassCastException){
             Log.e(TAG, "$context must implement DataStateChangeListener" )
+        }
+
+        try{
+            uiCommunicationListener = context as UICommunicationListener
+        }catch(e: ClassCastException){
+            Log.e(TAG, "$context must implement UICommunicationListener" )
         }
     }
 
